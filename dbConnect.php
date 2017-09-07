9c8f3e5f21c6c5d03f169613550b66f2c6dcf478
imran <30371031+imranlatur24@users.noreply.github.com>
2017-09-07 11:16:22 +0530

    db files
    db files using php

---
+++ dbConnect.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','root');
+ define('PASS','imran786');
+ define('DB','andylogin');
+ 
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
